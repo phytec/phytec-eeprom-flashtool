5cdeb0974596418ecdd3d5077f06dfd8bfcc3ef8
Janine Hagemann <j.hagemann@phytec.de>
2019-03-21 14:41:56 +0100

    phytec-eeprom-flashtool.py: Update to python3
    Move from python2 to python3 because python2 will be EOL in 2020.
    
    Signed-off-by: Janine Hagemann <j.hagemann@phytec.de>

--- phytec_eeprom_flashtool.py
+++ phytec_eeprom_flashtool.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python2
-# Copyright (C) 2017 PHYTEC America, LLC
+#!/usr/bin/env python3
+# Copyright (C) 2017-2019 PHYTEC America, LLC
 
 import argparse
-import ConfigParser
+import configparser
 import os
 import struct
 import sys
@@ -46,7 +46,7 @@ def i2c_eeprom_init():
 
 def eeprom_read(i2c_bus, i2c_dev, addr, num_bytes):
 	try:
-		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'r')
+		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'rb')
 		eeprom_file.seek(addr)
 		read_eeprom = eeprom_file.read(num_bytes)
 		eeprom_file.close()
@@ -56,7 +56,7 @@ def eeprom_read(i2c_bus, i2c_dev, addr, num_bytes):
 
 def eeprom_write(i2c_bus, i2c_dev, addr, string):
 	try:
-		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'w')
+		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'wb')
 		eeprom_file.seek(addr)
 		eeprom_file.write(string)
 		eeprom_file.flush()
@@ -72,7 +72,7 @@ def open_som_config():
 			cfg_path = \
 				os.path.dirname(os.path.realpath(sys.argv[0])) \
 				+ cfg_dir
-		config_file = open(cfg_path + args.som + '.ini')
+		config_file = open(cfg_path + args.som + '.ini','r')
 		cfg_parser.readfp(config_file)
 		config_file.close()
 	except IOError as err:
@@ -125,41 +125,41 @@ def check_eeprom_dict(eeprom_dict):
 		sys.exit('Read PHYTEC EEPROM SOM revision is out of bounds!')
 
 def print_eeprom_dict(eeprom_dict):
-	print
-	print '%s-%s.%s EEPROM contents and configuration [%s]:' % (args.som,
+	print()
+	print('%s-%s.%s EEPROM contents and configuration [%s]:' % (args.som,
 		eeprom_dict['kit_opt'][:-2], eeprom_dict['kit_opt'][-2:],
-		args.command)
-	print
-	print '%-20s\t%-40d' % ('API version', eeprom_dict['api_version'])
-	print '%-20s\t%-40d' % ('Mod version', eeprom_dict['mod_version'])
-	print '%-20s\t%-40d' % ('SOM PCB revision', eeprom_dict['som_pcb_rev'])
-	print '%-20s\t%-40d' % ('KSP style', eeprom_dict['ksp'])
-	print '%-20s\t%-40d' % ('KSP number', eeprom_dict['kspno'])
-	print '%-20s\t%-40d' % ('Bits set', eeprom_dict['bs'])
+		args.command))
+	print()
+	print('%-20s\t%-40d' % ('API version', eeprom_dict['api_version']))
+	print('%-20s\t%-40d' % ('Mod version', eeprom_dict['mod_version']))
+	print('%-20s\t%-40d' % ('SOM PCB revision', eeprom_dict['som_pcb_rev']))
+	print('%-20s\t%-40d' % ('KSP style', eeprom_dict['ksp']))
+	print('%-20s\t%-40d' % ('KSP number', eeprom_dict['kspno']))
+	print('%-20s\t%-40d' % ('Bits set', eeprom_dict['bs']))
 	mac_str = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (tuple(map(ord, eeprom_dict['mac'])))
-	print '%-20s\t%-40s' % ('MAC address', mac_str)
-	print
-	print 'Verbose kit options:'
+	print('%-20s\t%-40s' % ('MAC address', mac_str))
+	print()
+	print('Verbose kit options:')
 	for i in range(0, len(eeprom_dict['kit_opt'][:-2])):
 		kit_opt = cfg_parser.get('Kit', str(i))
 		opt_str = cfg_parser.get(kit_opt, eeprom_dict['kit_opt'][i])
-		print '%-20s\t%-40s' % (kit_opt, opt_str)
-	print
+		print('%-20s\t%-40s' % (kit_opt, opt_str))
+	print()
 
 def get_mac_addr():
 	eth_name = cfg_parser.get('PHYTEC', 'eth_name')
 	eth_sysfs = '/sys/class/net/%s/address' % (eth_name)
 	try:
-		mac_file = open(eth_sysfs)
+		mac_file = open(eth_sysfs, 'r', encoding='latin1')
 		mac_read = mac_file.read()
 		mac_file.close()
 		mac_read = mac_read.split(':')
-		mac_str = ''.join(map(lambda x: chr(int(x, base=16)), mac_read))
+		mac_str = ''.join([chr(int(x, base=16)) for x in mac_read])
 	except IOError as err:
-		print 'Failed to get MAC address. Using default of ' \
-			'00:00:00:00:00:00.'
+		print('Failed to get MAC address. Using default of ' \
+			'00:00:00:00:00:00.')
 		return '\x00\x00\x00\x00\x00\x00'
-	return mac_str
+	return mac_str.encode('utf-8')
 
 def count_eeprom_dict_bits(eeprom_dict):
 	count = 0
@@ -167,8 +167,7 @@ def count_eeprom_dict_bits(eeprom_dict):
 	count += bin(eeprom_dict['api_version']).count('1')
 	count += bin(eeprom_dict['mod_version']).count('1')
 	count += bin(eeprom_dict['som_pcb_rev']).count('1')
-	count += sum(map(lambda x: bin(ord(x)).count('1'),
-		eeprom_dict['mac']))
+	count += sum([bin(x).count('1') for x in eeprom_dict['mac']])
 	count += bin(eeprom_dict['ksp']).count('1')
 	count += bin(eeprom_dict['kspno']).count('1')
 	for i in eeprom_dict['kit_opt']:
@@ -180,8 +179,6 @@ def count_eeprom_dict_bits(eeprom_dict):
 	return count
 
 def dict_to_struct(eeprom_dict):
-	print 'dict_to_struct, kit_opt_str:', eeprom_dict['kit_opt']
-
 	eeprom_struct = struct.pack(
 		# format: uint, 3 uchars, 6-len str, 2 uchars, 11-len str,
 		# 5-len pad, 1 uchar
@@ -193,9 +190,10 @@ def dict_to_struct(eeprom_dict):
 		eeprom_dict['mac'],
 		eeprom_dict['ksp'],
 		eeprom_dict['kspno'],
-		eeprom_dict['kit_opt'],
+		bytes(eeprom_dict['kit_opt'],'utf-8'),
 		eeprom_dict['bs']
 	)
+
 	return eeprom_struct
 
 def struct_to_dict(eeprom_struct, eeprom_dict):
@@ -213,7 +211,7 @@ def struct_to_dict(eeprom_struct, eeprom_dict):
 	eeprom_dict['mac'] = unpacked[4]
 	eeprom_dict['ksp'] = unpacked[5]
 	eeprom_dict['kspno'] = unpacked[6]
-	eeprom_dict['kit_opt'] = unpacked[7]
+	eeprom_dict['kit_opt'] = unpacked[7].decode('utf-8')
 	eeprom_dict['bs'] = unpacked[8]
 
 def read_som_config():
@@ -239,9 +237,9 @@ def write_som_config():
 	read_eeprom = eeprom_read(i2c_cfg['i2c_bus'], i2c_cfg['i2c_dev'],
 		i2c_cfg['eeprom_offset'], eeprom_size)
 	if write_eeprom == read_eeprom:
-		print 'EEPROM flash successful!'
+		print('EEPROM flash successful!')
 	else:
-		print 'EEPROM flash failed!'
+		print('EEPROM flash failed!')
 
 def format_args():
 	try:
@@ -266,14 +264,14 @@ def main():
 		'verbose board configuration')
 	display_parser.add_argument('bom', help='PXX-###-####.X# (or KSX##.X#) format.')
 	display_parser.add_argument('som_pcb_rev', help='PCB Revision number.',
-		type=int, choices=xrange(0, 10))
+		type=int, choices=range(0, 10))
 	write_parser = arg_subparser.add_parser('write', help='Write EEPROM')
 	write_parser.add_argument('bom', help='PXX-###-####.X# (or KSX##.X#) format.')
 	write_parser.add_argument('som_pcb_rev', help='PCB Revision number.',
-		type=int, choices=xrange(0, 10))
+		type=int, choices=range(0, 10))
 
 	global cfg_parser
-	cfg_parser = ConfigParser.SafeConfigParser()
+	cfg_parser = configparser.SafeConfigParser()
 
 	global args
 	args = arg_parser.parse_args()
