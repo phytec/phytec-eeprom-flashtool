2561768589adda2cf92d9cfd7b79f7bf5c73802e
Janine Hagemann <j.hagemann@phytec.de>
2019-03-27 09:45:30 +0100

    phytec-eeprom-flashtool.py: Customize script for yaml configuration file
    Signed-off-by: Janine Hagemann <j.hagemann@phytec.de>

--- phytec_eeprom_flashtool.py
+++ phytec_eeprom_flashtool.py
@@ -2,10 +2,11 @@
 # Copyright (C) 2017-2019 PHYTEC America, LLC
 
 import argparse
-import configparser
 import os
 import struct
 import sys
+import yaml
+import binascii
 
 # Defaults defined by the PHYTEC EEPROM API version
 api_version = 0
@@ -14,7 +15,7 @@ eeprom_size = 32	# bytes
 max_kit_opts = 16
 min_bom_rev = 'A0'
 
-cfg_dir = '/configs/'
+yml_dir = '/configs/'
 
 # Global dicts for storing eeprom data and i2c settings
 ep = {
@@ -29,7 +30,7 @@ ep = {
 	'bs': None,
 }
 
-i2c_cfg = {
+i2c_yml = {
 	'i2c_bus': None,
 	'i2c_dev': None,
 	'eeprom_offset': None,
@@ -37,16 +38,17 @@ i2c_cfg = {
 }
 
 def i2c_eeprom_init():
-	i2c_cfg['i2c_bus'] = cfg_parser.getint('PHYTEC', 'i2c_bus')
-	i2c_cfg['i2c_dev'] = cfg_parser.getint('PHYTEC', 'i2c_dev')
-	i2c_cfg['eeprom_offset'] = cfg_parser.getint('PHYTEC', 'eeprom_offset')
-	i2c_cfg['eeprom_sysfs'] = \
-		'/sys/class/i2c-dev/i2c-%d/device/%d-%04d/eeprom' \
-		% (i2c_cfg['i2c_bus'], i2c_cfg['i2c_bus'], i2c_cfg['i2c_dev'])
+	i2c_yml['i2c_bus'] = yml_parser['PHYTEC']['i2c_bus']
+	i2c_yml['i2c_dev'] = yml_parser['PHYTEC']['i2c_dev']
+	i2c_yml['eeprom_offset'] = yml_parser['PHYTEC']['eeprom_offset']
+	i2c_yml['eeprom_sysfs'] = \
+		'/sys/class/i2c-dev/i2c-%d/device/%d-%s/eeprom' \
+		% (i2c_yml['i2c_bus'], i2c_yml['i2c_bus'],
+			i2c_yml['i2c_dev'].zfill(4))
 
 def eeprom_read(i2c_bus, i2c_dev, addr, num_bytes):
 	try:
-		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'rb')
+		eeprom_file = open(i2c_yml['eeprom_sysfs'], 'rb')
 		eeprom_file.seek(addr)
 		read_eeprom = eeprom_file.read(num_bytes)
 		eeprom_file.close()
@@ -56,7 +58,7 @@ def eeprom_read(i2c_bus, i2c_dev, addr, num_bytes):
 
 def eeprom_write(i2c_bus, i2c_dev, addr, string):
 	try:
-		eeprom_file = open(i2c_cfg['eeprom_sysfs'], 'wb')
+		eeprom_file = open(i2c_yml['eeprom_sysfs'], 'wb')
 		eeprom_file.seek(addr)
 		eeprom_file.write(string)
 		eeprom_file.flush()
@@ -65,15 +67,16 @@ def eeprom_write(i2c_bus, i2c_dev, addr, string):
 		sys.exit(err)
 
 def open_som_config():
+	global yml_parser
 	try:
 		if args.dir != None:
-			cfg_path = os.path.abspath(args.dir) + '/'
+			yml_path = os.path.abspath(args.dir) + '/'
 		else:
-			cfg_path = \
+			yml_path = \
 				os.path.dirname(os.path.realpath(sys.argv[0])) \
-				+ cfg_dir
-		config_file = open(cfg_path + args.som + '.ini','r')
-		cfg_parser.readfp(config_file)
+				+ yml_dir
+		config_file = open(yml_path + args.som + '.yml','r')
+		yml_parser = yaml.load(config_file)
 		config_file.close()
 	except IOError as err:
 		sys.exit(err)
@@ -81,7 +84,7 @@ def open_som_config():
 def load_som_config(eeprom_dict):
 	eeprom_dict['header'] = header
 	eeprom_dict['api_version'] = api_version
-	eeprom_dict['mod_version'] = cfg_parser.getint('PHYTEC', 'mod_version')
+	eeprom_dict['mod_version'] = yml_parser['PHYTEC']['mod_version']
 	eeprom_dict['som_pcb_rev'] = args.som_pcb_rev
 	eeprom_dict['mac'] = get_mac_addr()
 	eeprom_dict['kit_opt'] = args.options
@@ -89,12 +92,12 @@ def load_som_config(eeprom_dict):
 		eeprom_dict['ksp'] = 1
 		eeprom_dict['kspno'] = int(eeprom_dict['kit_opt'].split('KSP')[1])
 		eeprom_dict['kit_opt'] = \
-			cfg_parser.get('KSP', eeprom_dict['kit_opt'])
+			yml_parser['KSP'][eeprom_dict['kit_opt']]
 	elif eeprom_dict['kit_opt'][:3] == 'KSM':
 		eeprom_dict['ksp'] = 2
 		eeprom_dict['kspno'] = int(eeprom_dict['kit_opt'].split('KSM')[1])
 		eeprom_dict['kit_opt'] = \
-			cfg_parser.get('KSP', eeprom_dict['kit_opt'])
+			yml_parser['KSP'][eeprom_dict['kit_opt']]
 	else:
 		eeprom_dict['ksp'] = 0
 		eeprom_dict['kspno'] = 0
@@ -110,16 +113,15 @@ def check_eeprom_dict(eeprom_dict):
 		sys.exit('Read PHYTEC EEPROM PCB Revision is out of bounds!')
 	if eeprom_dict['ksp'] < 0 or eeprom_dict['ksp'] > 2:
 		sys.exit('Read PHYTEC EEPROM KSP identifier is out of bounds!')
-	if not cfg_parser.has_option('Known', eeprom_dict['kit_opt'][:-2]):
+	if not yml_parser['Known'][eeprom_dict['kit_opt'][:-2]]:
 		sys.exit('PHYTEC EEPROM kit options not valid within '
 			'configuration file!')
 	if eeprom_dict['ksp'] != 0:
 		ksp_str = 'KSP' if eeprom_dict['ksp'] == 1 else 'KSM'
 		ksp_str += '%02d' % (eeprom_dict['kspno'])
-		max_bom_rev = cfg_parser.get('Known', ksp_str)
+		max_bom_rev = yml_parser['Known'][ksp_str]
 	else:
-		max_bom_rev = cfg_parser.get('Known',
-			eeprom_dict['kit_opt'][:-2])
+		max_bom_rev = yml_parser['Known'][eeprom_dict['kit_opt'][:-2]]
 	if eeprom_dict['kit_opt'][-2:] < min_bom_rev \
 			or eeprom_dict['kit_opt'][-2:] > max_bom_rev:
 		sys.exit('Read PHYTEC EEPROM SOM revision is out of bounds!')
@@ -141,25 +143,25 @@ def print_eeprom_dict(eeprom_dict):
 	print()
 	print('Verbose kit options:')
 	for i in range(0, len(eeprom_dict['kit_opt'][:-2])):
-		kit_opt = cfg_parser.get('Kit', str(i))
-		opt_str = cfg_parser.get(kit_opt, eeprom_dict['kit_opt'][i])
+		kit_opt = yml_parser['Kit'][i]
+		opt_str = yml_parser[kit_opt][eeprom_dict['kit_opt'][i])]
 		print('%-20s\t%-40s' % (kit_opt, opt_str))
 	print()
 
 def get_mac_addr():
-	eth_name = cfg_parser.get('PHYTEC', 'eth_name')
+	eth_name = yml_parser['PHYTEC']['eth_name']
 	eth_sysfs = '/sys/class/net/%s/address' % (eth_name)
 	try:
-		mac_file = open(eth_sysfs, 'r', encoding='latin1')
+		mac_file = open(eth_sysfs, 'r')
 		mac_read = mac_file.read()
 		mac_file.close()
-		mac_read = mac_read.split(':')
-		mac_str = ''.join([chr(int(x, base=16)) for x in mac_read])
+		mac_read = mac_read.replace(':', '').strip()
+		mac_str = binascii.unhexlify(mac_read)
 	except IOError as err:
 		print('Failed to get MAC address. Using default of ' \
 			'00:00:00:00:00:00.')
 		return '\x00\x00\x00\x00\x00\x00'
-	return mac_str.encode('utf-8')
+	return mac_str
 
 def count_eeprom_dict_bits(eeprom_dict):
 	count = 0
@@ -216,8 +218,8 @@ def struct_to_dict(eeprom_struct, eeprom_dict):
 
 def read_som_config():
 	i2c_eeprom_init()
-	read_eeprom = eeprom_read(i2c_cfg['i2c_bus'], i2c_cfg['i2c_dev'],
-		i2c_cfg['eeprom_offset'], eeprom_size)
+	read_eeprom = eeprom_read(i2c_yml['i2c_bus'], i2c_yml['i2c_dev'],
+		i2c_yml['eeprom_offset'], eeprom_size)
 	struct_to_dict(read_eeprom, ep)
 	check_eeprom_dict(ep)
 	print_eeprom_dict(ep)
@@ -232,10 +234,10 @@ def write_som_config():
 	load_som_config(ep)
 	check_eeprom_dict(ep)
 	write_eeprom = dict_to_struct(ep)
-	eeprom_write(i2c_cfg['i2c_bus'], i2c_cfg['i2c_dev'],
-		i2c_cfg['eeprom_offset'], write_eeprom)
-	read_eeprom = eeprom_read(i2c_cfg['i2c_bus'], i2c_cfg['i2c_dev'],
-		i2c_cfg['eeprom_offset'], eeprom_size)
+	eeprom_write(i2c_yml['i2c_bus'], i2c_yml['i2c_dev'],
+		i2c_yml['eeprom_offset'], write_eeprom)
+	read_eeprom = eeprom_read(i2c_yml['i2c_bus'], i2c_yml['i2c_dev'],
+		i2c_yml['eeprom_offset'], eeprom_size)
 	if write_eeprom == read_eeprom:
 		print('EEPROM flash successful!')
 	else:
@@ -269,9 +271,6 @@ def main():
 	write_parser.add_argument('bom', help='PXX-###-####.X# (or KSX##.X#) format.')
 	write_parser.add_argument('som_pcb_rev', help='PCB Revision number.',
 		type=int, choices=range(0, 10))
-
-	global cfg_parser
-	cfg_parser = configparser.SafeConfigParser()
 
 	global args
 	args = arg_parser.parse_args()
