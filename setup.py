d7cda7220f39080e1108f978b23a478058ed4188
Daniel Schultz <d.schultz@phytec.de>
2020-10-24 12:40:44 +0200

    Add setuptools
    Allows to create this tool as a package.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+def get_version():
+    return '1.0.0'
+
+if __name__ == "__main__":
+    setup(
+        name='eeprom-flashtool',
+        version=get_version(),
+        description='PHYTEC EEPROM Flashtool',
+        packages=['src'],
+        author='PHYTEC America, LLC',
+        author_email='mmckee@phytec.com',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+        ],
+        python_requires='>=3.6',
+        install_requires=['pyyaml'],
+        entry_points={},
+        include_package_data=True,
+    )
