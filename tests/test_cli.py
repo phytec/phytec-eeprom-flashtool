0d4b951fd826ccd51daadde04f6b521dab28ee27
Janine Hagemann <j.hagemann@phytec.de>
2021-05-26 10:25:39 +0200

    tests: test_cli.py: Remove directory test
    The directory-change property is removed due the tool update.
    
    Signed-off-by: Janine Hagemann <j.hagemann@phytec.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,8 @@
 """Tests for the cli calls"""
 import subprocess
 
-
-def test_cli_directoy():
-    command = "python3 src/phytec_eeprom_flashtool.py -d src/../configs/ display PCL-066-3022210I.A0 1"
-    print(command)
-    result = subprocess.run(command.split(' '))
-    assert result.returncode == 0
-
 def test_cli_display():
-    command = "python3 src/phytec_eeprom_flashtool.py display PCL-066-3022210I.A0 1"
+    command = "python3 src/phytec_eeprom_flashtool.py -o display -som PCL-066 -kit 3022210I.A0 -rev 1"
     print(command)
     result = subprocess.run(command.split(' '))
     assert result.returncode == 0
