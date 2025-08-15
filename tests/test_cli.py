3e0451a619a767605de1933d98a8ed7bba97e048
Marc Dzierzawa <M.Dzierzawa@phytec.de>
2025-08-15 12:59:54 +0200

    Update configs to current option tree PCL-066,PCL-069,PCL/PCM-070
    Signed-off-by: Marc Dzierzawa <M.Dzierzawa@phytec.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,14 +16,14 @@ def test_cli_version():
     assert result.stdout.decode('utf-8').startswith('Version: ')
 
 def test_cli_display():
-    command = ['phytec_eeprom_flashtool', 'display', '-som', 'PCL-066', '-kit', '3022210I',
+    command = ['phytec_eeprom_flashtool', 'display', '-som', 'PCL-066', '-kit', '3022210I-0',
         '-bom', 'A0', '-pcb', '1a']
     print(" ".join(command))
     result = subprocess.run(command)
     assert result.returncode == 0
 
 def test_cli_create():
-    command = ['phytec_eeprom_flashtool', 'create', '-som', 'PCL-066', '-kit', '3022210I',
+    command = ['phytec_eeprom_flashtool', 'create', '-som', 'PCL-066', '-kit', '3022210I-0',
         '-bom', 'A0', '-pcb', '1a']
     print(" ".join(command))
     result = subprocess.run(command)
