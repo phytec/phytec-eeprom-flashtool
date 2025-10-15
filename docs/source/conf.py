e619bb79c69ab56ec6b66cfcbc50965c60e2de8c
Stefan Müller-Klieser <s.mueller-klieser@phytec.de>
2025-10-15 23:54:41 +0200

    doc: conf: update document template
    changes:
    - copy over the logo from docs repo
    - update html template
    - add latex template for pdf rendering
    - convert logo to png for latex
    
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,9 +44,63 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = '_static/logo-phytec.svg'
+html_css_files = ['theme.css']
+
+# -- Options for LaTeX to PDF output -----------------------------------------
+root_doc = 'index'
+latex_engine = 'pdflatex'  # or 'xelatex' or 'lualatex'
+
+latex_elements = {
+    # Paper size ('letterpaper' or 'a4paper')
+    'papersize': 'a4paper',
+
+    # Font size ('10pt', '11pt' or '12pt')
+    'pointsize': '11pt',
+
+    # Additional preamble content
+    'preamble': r'''
+        \usepackage{charter}
+        \usepackage[defaultsans]{lato}
+        \usepackage{inconsolata}
+    ''',
+
+    # Figure alignment
+    'figure_align': 'htbp',
+
+    # Custom title page
+    'maketitle': r'''
+        \begin{titlepage}
+            \centering
+            \includegraphics[width=0.4\textwidth]{logo-phytec.png}\par
+            \vspace*{2cm}
+            {\Huge\bfseries PHYTEC EEPROM Flashtool\par}
+            \vspace{1cm}
+            {\Large Documentation\par}
+            \vspace{2cm}
+            {\large Version ''' + release + r''' \par}
+            \vfill
+            {\large \today\par}
+        \end{titlepage}
+    ''',
+
+    # Table of contents depth
+    'tableofcontents': r'\sphinxtableofcontents',
+}
+
+# LaTeX document class
+latex_documents = [
+    (root_doc, 'phytec-eeprom-flashtool.tex', 'PHYTEC EEPROM Flashtool Documentation',
+     'support@phytec.de', 'manual'),
+]
+
+# Logo for LaTeX output
+latex_logo = '_static/logo-phytec.png'
+
+# Additional LaTeX styling
+latex_theme = 'manual'  # or 'howto'
